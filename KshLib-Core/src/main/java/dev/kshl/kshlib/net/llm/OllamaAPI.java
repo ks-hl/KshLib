b2404389eef00b7447d719bf013ba4a077951d58
ks-hl <8331595+ks-hl@users.noreply.github.com>
2025-01-15 21:17:37 -0500

    Cleanup code

--- KshLib-Core/src/main/java/dev/kshl/kshlib/net/llm/OllamaAPI.java
+++ KshLib-Core/src/main/java/dev/kshl/kshlib/net/llm/OllamaAPI.java
@@ -1,5 +1,6 @@
 package dev.kshl.kshlib.net.llm;
 
+import dev.kshl.kshlib.misc.TimeUtil;
 import dev.kshl.kshlib.net.HTTPRequestType;
 import dev.kshl.kshlib.net.NetUtil;
 import dev.kshl.kshlib.net.NetUtilInterval;
@@ -7,7 +8,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.util.List;
+import java.time.ZoneId;
 
 public class OllamaAPI extends NetUtilInterval {
 
@@ -16,45 +17,17 @@ public class OllamaAPI extends NetUtilInterval {
     }
 
     public LLMResponse generate(LLMRequest llmRequest) throws IOException {
-        String bodyResponse = null;
-        try {
-            NetUtil.Request request = new NetUtil.Request(adaptSuffixAndRateLimit("api/generate"), HTTPRequestType.POST, false);
-            request.body(llmRequest.toJSON().toString());
-            request.headers("content-type", "application/json");
-            request.timeout(llmRequest.getTimeout());
-            var response = request.request();
-            if (response.getResponseCode().isError()) {
-                throw new IOException(response.getResponseCode() + " (" + response.getResponseCode().getCode() + "): " + response.getBody());
-            }
-            bodyResponse = response.getBody();
-            JSONObject responseJSON = response.getJSON();
+        JSONObject responseJSON = request("api/generate", llmRequest);
 
-            try {
-                return LLMResponse.fromJSON(responseJSON);
-            } catch (JSONException e) {
-                throw new IOException("Invalid response: " + responseJSON, e);
-            }
+        try {
+            return LLMResponse.fromJSON(responseJSON);
         } catch (JSONException e) {
-            throw new JSONException("Body: " + bodyResponse, e);
+            throw new IOException("Invalid response: " + responseJSON, e);
         }
     }
 
     public EmbedResponse embeddings(EmbedRequest embedRequest) throws IOException {
-        NetUtil.Request request = new NetUtil.Request(adaptSuffixAndRateLimit("api/embed"), HTTPRequestType.POST, false);
-        request.body(embedRequest.toJSON().toString());
-        request.headers("content-type", "application/json");
-        request.timeout(embedRequest.getTimeout());
-        var response = request.request();
-        if (response.getResponseCode().isError()) {
-            throw new IOException(response.getResponseCode() + " (" + response.getResponseCode().getCode() + "): " + response.getBody());
-        }
-        String bodyResponse = response.getBody();
-        JSONObject responseJSON;
-        try {
-            responseJSON = response.getJSON();
-        } catch (JSONException e) {
-            throw new JSONException("Response not JSON. Body: " + bodyResponse, e);
-        }
+        JSONObject responseJSON = request("api/embed", embedRequest);
         try {
             return EmbedResponse.fromJSON(responseJSON);
         } catch (JSONException e) {
@@ -62,20 +35,36 @@ public class OllamaAPI extends NetUtilInterval {
         }
     }
 
-//    public static void main(String[] args) throws IOException {
-//        LLMResponse response = new OllamaAPI("http://10.0.70.105:11434/").embeddings(new LLMRequest("nomic-embed-text",
-//                "What's your favorite color?"));
-//        System.out.println(response.getTokensPerSecond() + " t/s");
-//        System.out.println(response.created_at().toInstant().toEpochMilli());
-//        System.out.println(System.currentTimeMillis());
-//        System.out.println(response.created_at().withZoneSameInstant(ZoneId.systemDefault()).format(TimeUtil.ENTRY_TIME_FORMAT));
-//        System.out.println(response);
-//    }
+    private JSONObject request(String endpoint, LLMRequest llmRequest) throws IOException {
+        NetUtil.Request request = new NetUtil.Request(adaptSuffixAndRateLimit(endpoint), HTTPRequestType.POST, false);
+        request.body(llmRequest.toJSON().toString());
+        request.headers("content-type", "application/json");
+        request.timeout(llmRequest.getTimeout());
+        var response = request.request();
+        if (response.getResponseCode().isError()) {
+            throw new IOException(response.getResponseCode() + " (" + response.getResponseCode().getCode() + "): " + response.getBody());
+        }
+        String bodyResponse = response.getBody();
+        try {
+            return response.getJSON();
+        } catch (JSONException e) {
+            throw new IOException("Response not JSON. Body: " + bodyResponse, e);
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         embed("What's your favorite color?");
         embed("What is your favorite colorrr??");
         embed("What's your favorite color?What's your favorite color?What's your favorite color?What's your favorite color?What's your favorite color?What's your favorite color?");
+
+
+        LLMResponse response = new OllamaAPI("http://10.0.70.105:11434/").generate(new LLMRequest("qwen2.5:3b",
+                "What's your favorite color?"));
+        System.out.println(response.getTokensPerSecond() + " t/s");
+        System.out.println(response.created_at().toInstant().toEpochMilli());
+        System.out.println(System.currentTimeMillis());
+        System.out.println(response.created_at().withZoneSameInstant(ZoneId.systemDefault()).format(TimeUtil.ENTRY_TIME_FORMAT));
+        System.out.println(response.response());
     }
 
     private static void embed(String str) throws IOException {
