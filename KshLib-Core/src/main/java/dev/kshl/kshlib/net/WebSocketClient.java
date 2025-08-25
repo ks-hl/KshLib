3fbc121b5f6df002ce744badf167d8ff1ccc5524
ks-hl <8331595+ks-hl@users.noreply.github.com>
2025-08-25 11:43:46 -0400

    Binary handling

--- KshLib-Core/src/main/java/dev/kshl/kshlib/net/WebSocketClient.java
+++ KshLib-Core/src/main/java/dev/kshl/kshlib/net/WebSocketClient.java
@@ -3,10 +3,17 @@ package dev.kshl.kshlib.net;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.WebSocket;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletionStage;
 
 public abstract class WebSocketClient implements WebSocket.Listener {
     private WebSocket webSocket;
+
+    private StringBuilder textBuffer = new StringBuilder();
+    private final List<ByteBuffer> binaryBuffer = new ArrayList<>();
+    private int binaryBufferTotalBytes = 0;
 
     public WebSocketClient(String url) {
         HttpClient.newHttpClient()
@@ -24,8 +31,6 @@ public abstract class WebSocketClient implements WebSocket.Listener {
         webSocket.sendText(message, true);
     }
 
-    private StringBuilder textBuffer = new StringBuilder();
-
     @Override
     public final CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
         try {
@@ -37,10 +42,38 @@ public abstract class WebSocketClient implements WebSocket.Listener {
         return WebSocket.Listener.super.onText(webSocket, data, last);
     }
 
+    @Override
+    public final CompletionStage<?> onBinary(WebSocket webSocket, ByteBuffer data, boolean last) {
+        try {
+            handle(data, last);
+        } catch (Throwable t) {
+            System.out.println("Uncaught exception handling binary data");
+            t.printStackTrace();
+        }
+        return WebSocket.Listener.super.onBinary(webSocket, data, last);
+    }
+
     private synchronized void handle(CharSequence data, boolean last) {
         textBuffer.append(data);
         if (!last) return;
         WebSocketClient.this.onText(textBuffer.toString());
         textBuffer = new StringBuilder();
+    }
+
+    private synchronized void handle(ByteBuffer data, boolean last) {
+        binaryBuffer.add(data.slice());
+        binaryBufferTotalBytes += data.remaining();
+        if (!last) return;
+        ByteBuffer complete = ByteBuffer.allocate(binaryBufferTotalBytes);
+        for (ByteBuffer buffer : binaryBuffer) {
+            complete.put(buffer);
+        }
+        binaryBuffer.clear();
+        binaryBufferTotalBytes = 0;
+        complete.flip();
+        onBinary(complete);
+    }
+
+    protected void onBinary(ByteBuffer data) {
     }
 }
