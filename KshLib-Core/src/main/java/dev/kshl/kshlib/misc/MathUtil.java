8c58a85a02df2f7f44659f261f3a230633818c05
ks-hl <8331595+ks-hl@users.noreply.github.com>
2025-09-05 13:23:05 -0400

    pow

--- KshLib-Core/src/main/java/dev/kshl/kshlib/misc/MathUtil.java
+++ KshLib-Core/src/main/java/dev/kshl/kshlib/misc/MathUtil.java
@@ -5,6 +5,20 @@ public class MathUtil {
         return i * i;
     }
 
+    public static int pow(int base, int exponent) {
+        if (exponent < 0) {
+            throw new IllegalArgumentException("Exponent must be >= 0");
+        }
+        if (exponent > 1000000) {
+            return (int) Math.pow(base, exponent);
+        }
+        int out = 1;
+        for (int i = 0; i < exponent; i++) {
+            out *= base;
+        }
+        return out;
+    }
+
     public static double sq(double d) {
         return d * d;
     }
